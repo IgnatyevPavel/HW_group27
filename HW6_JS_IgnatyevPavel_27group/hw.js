e76a670ff666e5ff3de93a20e1fb82d0af953e32
IgnatyevPavel <p4vel.qa@gmail.com>
2022-07-07 06:07:18 +0300

    HW#6 JS done

--- HW6_JS_IgnatyevPavel_27group/hw.js
+++ HW6_JS_IgnatyevPavel_27group/hw.js
@@ -59,3 +59,12 @@ let age_3 = 60;
 //  31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
 //  32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
 //  33. Иначе выводите “Technical work”.
+if (age_1 < age_2) {
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then ");
+} else if (age_1 >= age_2 && age_1 < age_3) {
+    console.log("Welcome  !");
+} else if (age_1  > age_3) {
+    console.log("Keep calm and look Culture channel");
+} else {
+    console.log("Technical work");
+}
